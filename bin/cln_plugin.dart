71fc34e871be94150c853cf4263d61a1330804eb
swaptr <swapnil06.st@gmail.com>
2022-08-02 01:54:56 +0530

    docs: update README

--- bin/cln_plugin.dart
+++ bin/cln_plugin.dart
@@ -1,12 +1,12 @@
-import 'package:cln_plugin/cln_plugin.dart';
+import 'package:cln_plugin/kraken.dart';
 
 void main() {
   /// To create a plugin in dart we need to extend the Plugin
   /// class and create an instance of the extended class. This
   /// instance will be used for any interaction with our plugin.
-  var plugin = MyPlugin();
+  var kraken = KrakenPlugin();
 
   /// The plugin start method reads for incoming requests on the
   /// stdin and outputs on the stdout over the JSON-RPCv2.0.
-  plugin.start();
+  kraken.start();
 }
--- lib/cln_plugin.dart
+++
@@ -1,6 +0,0 @@
-/// This is a template for plugins written in
-/// the dart programming environment.
-
-library cln_plugin;
-
-export 'src/cln_plugin.dart';
