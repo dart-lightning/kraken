58c55a5b82c2f9f8c4881f158b83d030d353ec5e
swaptr <swapnil06.st@gmail.com>
2022-08-02 02:30:29 +0530

    kraken: added rpc methods
    Co-authored-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- lib/src/kraken.dart
+++ lib/src/kraken.dart
@@ -1,33 +1,46 @@
 import 'package:cln_plugin_api/cln_plugin.dart';
 
 class KrakenPlugin extends Plugin {
+  /// This is the Kraken doctor RPC method. This method allows for
+  /// analyzing the failure of invoice payments.
   Future<Map<String, Object>> krakenDoctor(
       Plugin plugin, Map<String, Object> request) {
-    log(level: "info", message: "RPC Method call.");
+    log(level: "info", message: "This is the kraken doctor output.");
+    var params = HashMap<String, dynamic>{
+       "invoice": request["invoice"],
+    };
+    let listpays = rpc.call(method: "listpays", params: params);
+    /// Make a call to listfunds like the prev one, and take only the channels
     return Future.value({
-      "greeting": "Hello ${options["name"]!.value.toString()}!",
+          "listpays": listpays
+          "channels": channels
     });
   }
 
+  /// This is the Kraken pay RPC method. This method allows for
+  /// an alternate method to pay invoices in Core Lightning and
+  /// return payment failure analysis reports.
   Future<Map<String, Object>> krakenPay(
       Plugin plugin, Map<String, Object> request) {
-    log(level: "info", message: "RPC Method call.");
-    return Future.value({
-      "greeting": "Hello ${options["name"]!.value.toString()}!",
-    });
+    log(level: "info", message: "This is the kraken pay output.");
+// TODO: rpc should return an exception if any error occurs, and if we have the error returned run the doctor command
+   HashMap<String, dynamic> result = await rpc.call(method: "pay", params: PayInput.from(request));
+    return Future.value(result);
   }
+
   @override
   void configurePlugin() {
+    registerRPCMethod(
+        name: "kraken_doctor",
+        usage: "",
+        description:
+            "Statically analyze a payment and provide insights in the case of a failure.",
+        callback: krakenDoctor);
     registerRPCMethod(
         name: "kraken_pay",
         usage: "",
         description:
-            "This method will greet you and serves as an example of how to register a RPC method.",
+            "Wrap all the complexity to pay an invoice under one single command, and produce a failure report if the command fails.",
         callback: krakenPay);
-    registerRPCMethod(
-        name: "kraken_doctor",
-        usage: "",
-        description: "This method will analyze invoice ",
-        callback: krakenDoctor);
   }
 }
